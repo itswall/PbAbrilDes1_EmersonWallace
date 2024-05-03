5489c136d00e7c60d7c1cb0dc79468c3bef6e979
Wall <Wallaceknn@gmail.com>
2024-05-03 20:56:29 -0300

    Add method for alone child

--- Cenario2/src/park/FerrisWheel.java
+++ Cenario2/src/park/FerrisWheel.java
@@ -1,36 +1,61 @@
 package park;
 
 import entities.Adult;
+import entities.Child;
 
 public class FerrisWheel {
 
     Gondola[] seat = new Gondola[18];
 
     // method for alone adult
-    public void seat(int seatNum, Adult person){
+    public void seat(int seatNum, Adult person) {
 
-        if(seat[seatNum-1] != null){
-            if(seat[seatNum - 1].getSeatY() == null){
-                seat[seatNum-1].setSeatY(person);
-            }
-            else{
-                for(int i=0; i<seat.length; i++){
-                    if(seat[i] == null){
+        if (seat[seatNum - 1] != null) {
+            if (seat[seatNum - 1].getSeatY() == null) {
+                seat[seatNum - 1].setSeatY(person);
+            } else {
+                for (int i = 0; i < seat.length; i++) {
+                    if (seat[i] == null) {
                         seat[i] = new Gondola();
                         seat[i].setNum(seatNum);
                         seat[i].setSeatY(person);
-                    }
-                    else if(seat[i].getSeatY() == null){
+                    } else if (seat[i].getSeatY() == null) {
                         seat[i].setNum(seatNum);
                         seat[i].setSeatY(person);
                     }
                 }
             }
+        } else {
+            seat[seatNum - 1] = new Gondola();
+            seat[seatNum - 1].setNum(seatNum);
+            seat[seatNum - 1].setSeatY(person);
         }
-        else{
-            seat[seatNum-1] = new Gondola();
-            seat[seatNum-1].setNum(seatNum);
-            seat[seatNum-1].setSeatY(person);
+    }
+
+    //method for alone child
+    public void seat(int seatNum, Child person) {
+
+        if (person.getAge() < 12) {
+            seat[seatNum - 1] = null;
+        } else if (seat[seatNum - 1] != null) {
+            if (seat[seatNum - 1].getSeatY() == null) {
+                seat[seatNum - 1].setSeatY(person);
+            } else {
+                for (int i = 0; i < seat.length; i++) {
+                    if (seat[i] == null) {
+                        seat[i] = new Gondola();
+                        seat[i].setNum(seatNum);
+                        seat[i].setSeatY(person);
+                    } else if (seat[i].getSeatY() == null) {
+                        seat[i].setNum(seatNum);
+                        seat[i].setSeatY(person);
+                    }
+                }
+            }
+        } else {
+            seat[seatNum - 1] = new Gondola();
+            seat[seatNum - 1].setNum(seatNum);
+            seat[seatNum - 1].setSeatY(person);
         }
     }
 }
