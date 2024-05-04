7e257f9febc67a83d583c901b8c1f510fe23d8e6
Wall <Wallaceknn@gmail.com>
2024-05-03 23:32:24 -0300

    Add method status of the ferris wheel

--- Cenario2/src/park/FerrisWheel.java
+++ Cenario2/src/park/FerrisWheel.java
@@ -8,54 +8,54 @@ public class FerrisWheel {
     Gondola[] seat = new Gondola[18];
 
     // method for alone adult
-    public void seat(int seatNum, Adult person) {
+    public void seat(int seatNum, Adult personX) {
 
         if (seat[seatNum - 1] != null) {
             if (seat[seatNum - 1].getSeatY() == null) {
-                seat[seatNum - 1].setSeatY(person);
+                seat[seatNum - 1].setSeatY(personX);
             } else {
                 for (int i = 0; i < seat.length; i++) {
                     if (seat[i] == null) {
                         seat[i] = new Gondola();
                         seat[i].setNum(seatNum);
-                        seat[i].setSeatY(person);
+                        seat[i].setSeatY(personX);
                     } else if (seat[i].getSeatY() == null) {
                         seat[i].setNum(seatNum);
-                        seat[i].setSeatY(person);
+                        seat[i].setSeatY(personX);
                     }
                 }
             }
         } else {
             seat[seatNum - 1] = new Gondola();
             seat[seatNum - 1].setNum(seatNum);
-            seat[seatNum - 1].setSeatY(person);
+            seat[seatNum - 1].setSeatY(personX);
         }
     }
 
     //method for alone child
-    public void seat(int seatNum, Child person) {
+    public void seat(int seatNum, Child personX) {
 
-        if (person.getAge() < 12) {
+        if (personX.getAge() < 12) {
             seat[seatNum - 1] = null;
         } else if (seat[seatNum - 1] != null) {
             if (seat[seatNum - 1].getSeatY() == null) {
-                seat[seatNum - 1].setSeatY(person);
+                seat[seatNum - 1].setSeatY(personX);
             } else {
                 for (int i = 0; i < seat.length; i++) {
                     if (seat[i] == null) {
                         seat[i] = new Gondola();
                         seat[i].setNum(seatNum);
-                        seat[i].setSeatY(person);
+                        seat[i].setSeatY(personX);
                     } else if (seat[i].getSeatY() == null) {
                         seat[i].setNum(seatNum);
-                        seat[i].setSeatY(person);
+                        seat[i].setSeatY(personX);
                     }
                 }
             }
         } else {
             seat[seatNum - 1] = new Gondola();
             seat[seatNum - 1].setNum(seatNum);
-            seat[seatNum - 1].setSeatY(person);
+            seat[seatNum - 1].setSeatY(personX);
         }
     }
 
@@ -105,5 +105,20 @@ public class FerrisWheel {
             seat[seatNum - 1].setSeatY(personY);
         }
     }
-    
+
+    public void status(){
+        System.out.println("------------ Ferris Wheel Status ------------");
+
+        for(int i = 0; i<seat.length; i++){
+            if(seat[i] == null){
+                System.out.println("Seat " + i + " is empty");
+            }
+            else if(seat[i].getSeatY() == null){
+                System.out.println(seat[i].getSeatX().getName() + "Is alone :(");
+            }
+            else{
+                System.out.println(seat[i].getSeatX().getName() + "And" + seat[i].getSeatY().getName());
+            }
+        }
+    }
 }
